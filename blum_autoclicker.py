247bd5ee7c2f64915c4618d8fdcf421665aa4950
AlexMiniReal <aledelpiero25@gmail.com>
2024-06-28 16:18:35 +0200

    added functionality

--- blum_autoclicker.py
+++ blum_autoclicker.py
@@ -20,13 +20,20 @@ def get_window(window_name):
     except IndexError:
         return None # if the window is not found
 
+def is_window_none(window):
+    if window is None:
+        print()
+        input("Window not found. Press ENTER to exit.")
+        exit(1)
+
 # function to get the choice of the user
 def show_menu():
-    print("Select the window you want to use:")
     print("1: TelegramDesktop")
 
+# pyautogui.displayMousePosition()
+
 show_menu()
-choice = input("Enter the number of the window you want to use: ")
+choice = input("Select the window you want to use: ")
 print()
 if choice == "1":
     window_name = "TelegramDesktop"
@@ -35,14 +42,10 @@ else:
     exit(1)
 
 window = get_window(window_name)
-if window is None:
-    input("Window not found. Press ENTER to exit.")
-    exit(1)
-
-# pyautogui.displayMousePosition()
+is_window_none(window)
 
 window_left, window_top, window_width, window_height = window.left, window.top, window.width, window.height
-x_edit, y_edit, width_edit, height_edit, restart_button_x, restart_button_y = 9, 70, -18, -120, 50, -90
+x_edit, y_edit, width_edit, height_edit, restart_button_x, restart_button_y = 9, 70, -18, -120, int(window_width/2), -85
 x, y, width, height = window_left+x_edit, window_top+y_edit, window_width+width_edit, window_height+height_edit
 
 sleep_time = 1
@@ -61,9 +64,7 @@ while True:
             print(" [RESUMED] - Program resumed. Press 'q' to pause.")
             # get the window again in case it was moved
             window = get_window(window_name)
-            if window is None:
-                input("Window not found. Press ENTER to exit.")
-                exit(1)
+            is_window_none(window)
             window_left, window_top, window_width, window_height = window.left, window.top, window.width, window.height
             x, y, width, height = window_left+x_edit, window_top+y_edit, window_width+width_edit, window_height+height_edit
         time.sleep(0.5)
@@ -72,6 +73,9 @@ while True:
         # current_dir = os.path.dirname(os.path.abspath(__file__))
         # iml = pyautogui.screenshot(region=(x, y, width, height))
         # iml.save(r"{current_dir}\screenshot.png")
+
+        window = get_window(window_name)
+        is_window_none(window)
         
         pic = pyautogui.screenshot(region=(x, y, width, height))
         width, height = pic.size
@@ -87,5 +91,7 @@ while True:
             prev_mouse_pos = pyautogui.position()
             mouse_stationary_start = time.time()
         elif time.time() - mouse_stationary_start > 2: # if the mouse has not moved for 2 seconds, click the restart button
-            click(window.left+restart_button_x, window.bottom+restart_button_y)
+            restart_button_color = pyautogui.pixel(window.left+restart_button_x, window.bottom+restart_button_y)
+            if restart_button_color == (255, 255, 255):
+                click(window.left+restart_button_x, window.bottom+restart_button_y)
             mouse_stationary_start = time.time()
