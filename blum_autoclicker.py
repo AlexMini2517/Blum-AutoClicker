49520c2c0b40e7679387e878cde56647e9f8098c
AlexMiniReal <aledelpiero25@gmail.com>
2024-07-26 22:07:37 +0200

    better readability

--- blum_autoclicker.py
+++ blum_autoclicker.py
@@ -3,7 +3,6 @@ try:
     # import os
     import pyautogui
     import pygetwindow as gw
-    import pyscreeze
     import random
     import time
     import win32api
@@ -19,11 +18,6 @@ def click(x, y):
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
 
-# function to check if the mouse has moved
-def has_mouse_moved(prev_pos):
-    current_pos = pyautogui.position()
-    return current_pos != prev_pos
-
 # function to get the window of the application
 def get_window(window_name):
     try:
@@ -34,7 +28,7 @@ def get_window(window_name):
 
 def is_window_none(window):
     if window is None:
-        input("Window not found. Press ENTER to exit...")
+        input("Window 'TelegramDesktop' not found. Press ENTER to exit...")
         exit(1)
 
 # pyautogui.displayMousePosition()
@@ -43,11 +37,11 @@ window = get_window(window_name)
 is_window_none(window)
 
 window_left, window_top, window_width, window_height = window.left, window.top, window.width, window.height
-x_edit, y_edit, width_edit, height_edit, restart_button_x, restart_button_y = 9, 150, -18, -200, int(window_width/2), -85
-x, y, width, height = window_left+x_edit, window_top+y_edit, window_width+width_edit, window_height+height_edit
+x_edit, y_edit, width_edit, height_edit, restart_button_x, restart_button_y = 9, 150, -18, -200, int(window_width / 2), -85
+x, y, width, height = window_left + x_edit, window_top + y_edit, window_width + width_edit, window_height + height_edit
 
 sleep_time = 1
-print(f"[STARTING] - The program will start in {sleep_time} seconds. Press 'q' to pause/resume.")
+print(f"[STARTING] - The program will start in {sleep_time} seconds. Press 'q' to activate/pause.")
 time.sleep(sleep_time)
 
 paused = False
@@ -55,15 +49,15 @@ while True:
     if keyboard.is_pressed('q'):
         paused = not paused # one time it pauses, the other time it resumes
         if paused:
-            print("  [PAUSED] - Program paused. Press 'q' to resume.")
+            print("[PAUSED] - Program paused. Press 'q' to activate.")
         else:
-            print(" [RESUMED] - Program resumed. Press 'q' to pause.")
-            # get the window again in case it was moved
+            print("[ACTIVE] - Program active. Press 'q' to pause.")
+            # refresh the window status and coordinates
             window = get_window(window_name)
             is_window_none(window)
             window_left, window_top, window_width, window_height = window.left, window.top, window.width, window.height
             x, y, width, height = window_left+x_edit, window_top+y_edit, window_width+width_edit, window_height+height_edit
-        time.sleep(0.5)
+        time.sleep(0.1)
 
     if paused:
         time.sleep(0.1)
@@ -74,10 +68,10 @@ while True:
         # time.sleep(1)
 
         try:
-            restart_button_color = pyautogui.pixel(window.left+restart_button_x, window.bottom+restart_button_y)
+            restart_button_color = pyautogui.pixel(window.left + restart_button_x, window.bottom + restart_button_y)
             if restart_button_color == (255, 255, 255):
                 time.sleep(0.5) # sleep to not click the restart button too fast
-                click(window.left+restart_button_x, window.bottom+restart_button_y)
+                click(window.left + restart_button_x, window.bottom + restart_button_y)
 
             pic = pyautogui.screenshot(region=(x, y, width, height))
             width, height = pic.size
@@ -87,6 +81,6 @@ while True:
                     if r == 205 and g == 220:
                         click(i + x, j + y)
                         time.sleep(0.001) # sleep to prevent too much CPU usage
-                        break
+                        j = height
         except:
             is_window_none(window)
